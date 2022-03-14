20f5ffde071f576d1da77b1355e0835b745b7879
Sarthak Sharma <ssarthak770@gmail.com>
2022-03-14 11:05:52 +0530

    Wrote my side of code

---
+++ script.js
@@ -0,0 +1,75 @@
+const screens = document.querySelectorAll('.screen');
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
+const start_btn = document.getElementById('start-btn');
+const game_container = document.querySelector('.game-container')
+const timeEl = document.getElementById('time');
+const scoreEl = document.getElementById('score');
+const message = document.getElementById('message');
+let seconds = 0;
+let score = 0;
+
+let selected_insect = {};
+
+start_btn.addEventListener('click', () => screens[0].classList.add('up'))
+
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img');
+        const src = img.getAttribute('src');
+        const alt = img.getAttribute('alt');
+        selected_insect = { src, alt };
+        screens[1].classList.add('up')
+        setTimeout(createInsect, 1000);
+        startGame();
+    })
+})
+function startGame() {
+    setInterval(increaseTime, 1000);
+}
+function createInsect() {
+    const insect = document.createElement('div');
+    insect.classList.add('insect');
+    const { x, y } = getRandomLocation();
+    insect.innerHTML = `<img src = "${selected_insect.src}" alt = "${selected_insect.alt}" style="transform:rotate(${Math.random() * 360}deg)">`
+    insect.style.top = `${y}px`;
+    insect.style.left = `${x}px`;
+
+    insect.addEventListener("click", catchInsect)
+    game_container.appendChild(insect);
+}
+
+function getRandomLocation() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const x = Math.random() * (width - 200) + 100
+    const y = Math.random() * (height - 200) + 100
+
+    return { x, y };
+}
+function catchInsect() {
+    increaseScore();
+    this.classList.add('caught');
+    setTimeout(() => this.remove(), 2000)
+    addInsect();
+}
+
+function addInsect(){
+    setTimeout(createInsect,900);
+    setTimeout(createInsect,1700);
+}
+
+function increaseTime() {
+    let min = Math.floor(seconds / 60);
+    let sec = seconds % 60;
+    min = min < 10 ? `0${min}` : min
+    sec = sec < 10 ? `0${sec}` : sec
+    timeEl.innerHTML = `Time: ${min}:${sec}`;
+    seconds++;
+}
+function increaseScore() {
+    score++;
+    if (score > 19) {
+        message.classList.add('visible');
+    }
+    scoreEl.innerHTML = `Score:${score}`
+}
